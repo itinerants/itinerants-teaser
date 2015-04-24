7ba169cc5773b2ca98974a6448596b89e9aa9f73
Abby S. Nugraha <abby.sukma@acommerce.asia>
2015-04-24 09:03:00 +0700

    add botstraps & modernizr to page

---
+++ js/itinerants.js
@@ -0,0 +1,13 @@
+/*
+	Itinerants(tm) production
+	inhouse developing project for : Teaser page of Itinerants front website
+	Any component, content and also script are copyrighted for Itinerants(tm) purpose only.
+	Any other use not involving Itinerants(tm) or not for any Itinerants(tm) purpose must be permited by any owner of Itinerants(tm) name.
+
+	involved dev : 
+	- a88y@github.com
+
+	any bug and issue, contact : tech.team@itinerants.co /abby@itinerants.co /  abby.s.nugraha@gmail.com
+*/
+
+$('.bgbig').tubular({videoId: 'cauCoaJWNyU'});
